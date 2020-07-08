cbe641feef271b7a4cc6c0c124fed4ea3dfc40a2
Jesus Jimenez <jalejandrojb2@gmail.com>
2020-07-08 15:45:51 -0400

    se agrega el script del cambio de numero del dado cada vez que se lanza

--- scripts.js
+++ scripts.js
@@ -1,24 +1,31 @@
-class Ludo {
+var i = 1;
 
+class Ludo {
+	
 	constructor() {
 		this.botonJugar = document.getElementById('BotonInicio')
+		this.dado = document.getElementById('dado')
 		this.ocultarBoton()
+		this.habilitarDado()
 	}
 
 	ocultarBoton() {
 		this.botonJugar.style.display="none"
 	}
+
+	habilitarDado() {
+		this.dado.addEventListener('click', this.tirarDado)
+	}
+
+	tirarDado() {
+		let x = Math.floor(Math.random() * 6) + 1
+		console.log(this)
+		this.innerHTML = x /*COMENTARIO: No logro acceder al DOM con la variable "dado", a laque se le asigno elelemento del dado*/
+		return x;
+
+	}
 }
 
 function empezarPartida() {
-	const partida = new Ludo()
+	const juego = new Ludo()
 }
-
-
-
-/*var casillas = [];
-
-for(i = 0; i < 78; i++) {
-	casillas[i] = document.getElementById('casilla' + (i + 1))
-}*/
-
